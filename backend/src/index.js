ea3dc5873a3592f7904fd5ddc655982203db988a
Josh Loftis <jshlfts32@gmail.com>
2018-11-11 23:17:26 -0500

    Frontend signup/login/logout

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,25 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
-// TODO Use express middlware to populate current user
+server.express.use(cookieParser());
+
+// Decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  // 1. Pull the token out of the request
+  const { token } = req.cookies;
+  // 2. Decode the token
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId on the request for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
