d07dc8e50e3f1ecb4dca1f4c3d9d7c7d2ae1d79c
Josh Loftis <jshlfts32@gmail.com>
2018-11-13 17:28:56 -0500

    update

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://sick-fits-prod-2fdcf6d7d9.herokuapp.com/`;
 export const perPage = 4;
